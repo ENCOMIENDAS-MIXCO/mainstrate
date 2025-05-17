e5dab46017f7019a93cf3dfbaf9db6079c006c1b
encomiendasmixco <encomiendasmixco@gmail.com>
2025-05-17 16:16:00 +0200

    add files via upload
    added paqueteria for review, contacto

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 /* Description: Custom JS file */
 
 /* Navigation*/
-
-
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
 
 window.onload = function () {
 	scrollFunction();
@@ -10,9 +13,9 @@ window.onload = function () {
 
 function scrollFunction() {
 	if (document.documentElement.scrollTop > 30) {
-		document.getElementById("navbarExample").classList.add("top-nav-collapse");
+		document.getElementById("navbarMain").classList.add("top-nav-collapse");
 	} else if ( document.documentElement.scrollTop < 30 ) {
-		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+		document.getElementById("navbarMain").classList.remove("top-nav-collapse");
 	}
 }
 
@@ -86,15 +89,45 @@ if (checkReplace !== null) {
 }
   
 
-
-
+/* Card Slider - Swiper */
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 3,
+	spaceBetween: 70,
+	breakpoints: {
+		// when window is <= 767px
+		767: {
+			slidesPerView: 1
+		},
+		// when window is <= 991px
+		991: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
 
 
 /* Back To Top Button */
 // Get the button
 myButton = document.getElementById("myBtn");
 
-
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
